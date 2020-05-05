f49f222ab582a3bb9b1c17096b5a7661390fe52c
tb59 <thomas.bourdeaudhuy@gmail.com>
2020-05-05 11:39:41 +0200

    Toast sur clib Btn

--- app/src/main/java/com/example/tp1_app2/MainActivity.java
+++ app/src/main/java/com/example/tp1_app2/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.tp1_app2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String TAG = "PMR";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    void alerter(String s) {
+        Log.i(TAG,s);
+        Toast t = Toast.makeText(this,s, Toast.LENGTH_LONG);
+        t.show();
+    }
+
+
+    public void foo(View view) {
+        alerter("Click sur OK");
     }
 }
